12a27763ddaf9fdd2ac4f0aaf9adf2e6348df715
sam <sam12306110@gmail.com>
2018-04-11 11:34:06 +0800

    update

--- src/components/basketball/drugAddiction.vue
+++ src/components/basketball/drugAddiction.vue
@@ -27,17 +27,19 @@
                 <th>单双</th>
                 <th colspan="2">球队得分：大/小</th>
               </tr>
-              <tr><td colspan="10" class="b_title">韩国篮球联赛</td></tr>
+              <tr>
+                <td colspan="10" class="b_title">韩国篮球联赛</td>
+              </tr>
               <tr class="b_cen">
                 <td rowspan="2" style=" white-space:nowrap;">
                   03-20<br>06:00<div class="roll">滚球</div>                        </td>
                 <td rowspan="2" align="left">全州KCC宙斯盾<br>仁川东土大象 </td>
                 <td>&nbsp;</td>
                 <td align="right">
-                  <span class="qiu">5.5</span><a href="javascript:" onclick="parent.orderFrame.selectMatch(&quot;2449022&quot;,&quot;bb&quot;,&quot;ds&quot;,&quot;rq_h&quot;)" title="全州KCC宙斯盾"><font>0.74</font></a>                        </td>
+                  <span class="qiu">5.5</span><a href="javascript:" title="全州KCC宙斯盾"><font>0.74</font></a>                        </td>
                 <td align="right">
                   <span class="qiu">大166.5</span>
-                  <a href="javascript:" onclick="parent.orderFrame.selectMatch(&quot;2449022&quot;,&quot;bb&quot;,&quot;ds&quot;,&quot;dx_h&quot;)" title="大"><font>1.03</font></a>                        </td>
+                  <a href="javascript:" title="大"><font>1.03</font></a>                        </td>
                 <td>
                 </td>
                 <td>
@@ -50,10 +52,10 @@
               <tr class="b_cen">
                 <td>&nbsp;</td>
                 <td align="right">
-                  <a href="javascript:" onclick="parent.orderFrame.selectMatch(&quot;2449022&quot;,&quot;bb&quot;,&quot;ds&quot;,&quot;rq_c&quot;)" title="仁川东土大象"><font>1.14</font></a>                        </td>
+                  <a href="javascript:"  title="仁川东土大象"><font>1.14</font></a>                        </td>
                 <td align="right">
                   <span class="qiu">小166.5</span>
-                  <a href="javascript:" onclick="parent.orderFrame.selectMatch(&quot;2449022&quot;,&quot;bb&quot;,&quot;ds&quot;,&quot;dx_c&quot;)" title="小"><font>0.83</font></a>                        </td>
+                  <a href="javascript:" title="小"><font>0.83</font></a>                        </td>
                 <td>
                 </td>
                 <td>
@@ -69,10 +71,10 @@
                 <td rowspan="2" align="left">全州KCC宙斯盾<br>仁川东土大象 </td>
                 <td>&nbsp;</td>
                 <td align="right">
-                  <span class="qiu">7.5</span><a href="javascript:" onclick="parent.orderFrame.selectMatch(&quot;2449015&quot;,&quot;bb&quot;,&quot;ds&quot;,&quot;rq_h&quot;)" title="全州KCC宙斯盾"><font>1.04</font></a>                        </td>
+                  <span class="qiu">7.5</span><a href="javascript:"  title="全州KCC宙斯盾"><font>1.04</font></a>                        </td>
                 <td align="right">
                   <span class="qiu">大164.5</span>
-                  <a href="javascript:" onclick="parent.orderFrame.selectMatch(&quot;2449015&quot;,&quot;bb&quot;,&quot;ds&quot;,&quot;dx_h&quot;)" title="大"><font>0.83</font></a>                        </td>
+                  <a href="javascript:" title="大"><font>0.83</font></a>                        </td>
                 <td>
                 </td>
                 <td>
@@ -85,10 +87,11 @@
               <tr class="b_cen">
                 <td>&nbsp;</td>
                 <td align="right">
-                  <a href="javascript:" onclick="parent.orderFrame.selectMatch(&quot;2449015&quot;,&quot;bb&quot;,&quot;ds&quot;,&quot;rq_c&quot;)" title="仁川东土大象"><font>0.84</font></a>                        </td>
+                  <a href="javascript:"  title="仁川东土大象"><font>0.84</font></a>                        </td>
                 <td align="right">
                   <span class="qiu">小164.5</span>
-                  <a href="javascript:" onclick="parent.orderFrame.selectMatch(&quot;2449015&quot;,&quot;bb&quot;,&quot;ds&quot;,&quot;dx_c&quot;)" title="小"><font>1.03</font></a>                        </td>
+                  <a href="javascript:" title="小"><font>1.03</font></a>
+                </td>
                 <td>
                 </td>
                 <td>
@@ -102,14 +105,15 @@
                 <td rowspan="2" style=" white-space:nowrap;">
                   03-20<br>06:00                        </td>
                 <td rowspan="2" align="left">全州KCC宙斯盾 - (第1节)<br>仁川东土大象 - (第1节) </td>
-                <td><a href="javascript:" onclick="parent.orderFrame.selectMatch(&quot;2449011&quot;,&quot;bb&quot;,&quot;ds&quot;,&quot;dy_h&quot;)" title="全州KCC宙斯盾 - (第1节)"><font>1.48</font></a></td>
+                <td><a href="javascript:" title="全州KCC宙斯盾 - (第1节)"><font>1.48</font></a></td>
                 <td align="right">
-                  <span class="qiu">2.5</span><a href="javascript:" onclick="parent.orderFrame.selectMatch(&quot;2449011&quot;,&quot;bb&quot;,&quot;ds&quot;,&quot;rq_h&quot;)" title="全州KCC宙斯盾 - (第1节)"><font>0.84</font></a>                        </td>
+                  <span class="qiu">2.5</span><a href="javascript:"  title="全州KCC宙斯盾 - (第1节)"><font>0.84</font></a>
+                </td>
                 <td align="right">
                   <span class="qiu">大40.5</span>
-                  <a href="javascript:" onclick="parent.orderFrame.selectMatch(&quot;2449011&quot;,&quot;bb&quot;,&quot;ds&quot;,&quot;dx_h&quot;)" title="大"><font>0.90</font></a>                        </td>
+                  <a href="javascript:" title="大"><font>0.90</font></a>                        </td>
                 <td>
-                  <span class="qiu">单</span><a href="javascript:" onclick="parent.orderFrame.selectMatch(&quot;2449011&quot;,&quot;bb&quot;,&quot;ds&quot;,&quot;ds_h&quot;)" title="单"><font>1.91</font></a>                        </td>
+                  <span class="qiu">单</span><a href="javascript:"  title="单"><font>1.91</font></a>                        </td>
                 <td>
                   <span class="qiu"></span>
                   &nbsp;                        </td>
@@ -118,14 +122,14 @@
                   &nbsp;                        </td>
               </tr>
               <tr class="b_cen">
-                <td><a href="javascript:" onclick="parent.orderFrame.selectMatch(&quot;2449011&quot;,&quot;bb&quot;,&quot;ds&quot;,&quot;dy_c&quot;)" title="仁川东土大象 - (第1节)"><font>2.57</font></a></td>
+                <td><a href="javascript:" title="仁川东土大象 - (第1节)"><font>2.57</font></a></td>
                 <td align="right">
-                  <a href="javascript:" onclick="parent.orderFrame.selectMatch(&quot;2449011&quot;,&quot;bb&quot;,&quot;ds&quot;,&quot;rq_c&quot;)" title="仁川东土大象 - (第1节)"><font>1.02</font></a>                        </td>
+                  <a href="javascript:" title="仁川东土大象 - (第1节)"><font>1.02</font></a>                        </td>
                 <td align="right">
                   <span class="qiu">小40.5</span>
-                  <a href="javascript:" onclick="parent.orderFrame.selectMatch(&quot;2449011&quot;,&quot;bb&quot;,&quot;ds&quot;,&quot;dx_c&quot;)" title="小"><font>0.94</font></a>                        </td>
+                  <a href="javascript:"title="小"><font>0.94</font></a>                        </td>
                 <td>
-                  <span class="qiu">双</span><a href="javascript:" onclick="parent.orderFrame.selectMatch(&quot;2449011&quot;,&quot;bb&quot;,&quot;ds&quot;,&quot;ds_c&quot;)" title="双"><font>1.91</font></a>                        </td>
+                  <span class="qiu">双</span><a href="javascript:" title="双"><font>1.91</font></a>                        </td>
                 <td>
                   <span class="qiu"></span>
                   &nbsp;                        </td>
@@ -138,7 +142,7 @@
             </table>
           </div>
         </div>
-        <div id="refresh_down" class="refresh_M_btn" onclick="this.className='refresh_M_on';javascript:refload();">
+        <div id="refresh_down" class="refresh_M_btn" >
           <span>刷新</span>
         </div>
       </div>
@@ -152,13 +156,15 @@
         mixins: [Mixin],
         components: {},
         data: function () {
-            return {}
+            return {
+              action:'../api/app/member/show/json/bk_2_1.php',
+            }
         },
         created: function () {
 
         },
         mounted: function () {
-
+          this.getData()
         },
         methods: {}
     }
--- src/components/football/single.vue
+++ src/components/football/single.vue
@@ -40,65 +40,67 @@
                         {{items.Match_Master}}<br>{{items.Match_Guest}}
                       </td>
                       <td align="right" style="width: 38px;">
-                        <a href="javascript:"  title=""><font>{{items.Match_BzM}}</font></a>
+                        <a href="javascript:"  :title="items.Match_BzM" v-if="items.Match_BzM!=null&&items.Match_BzM!='0'"><font>{{items.Match_BzM}}</font></a>
                       </td>
                       <td align="right" style="width: 93px;">
-                        <a href="javascript:"  title=""><font>{{items.Match_Ho}}</font></a>
+                        <span class="qiu" v-if="items.Match_Ho!=null&&items.Match_Ho!='0'">{{items.Match_ShowType == 'H' && items.Match_Ho != '0' ? items.Match_RGG : ''}}</span>
+                        <a href="javascript:"  v-if="items.Match_Ho!=null&&items.Match_Ho!='0'"><font>{{items.Match_Ho}}</font></a>
                       </td>
                       <td align="right" style="width: 93px;">
-                        <span class="qiu">{{items.Match_DxGG1}}</span>
-                        <a href="javascript:"title="大"><font>{{items.Match_DxDpl}}</font></a>
+                        <span class="qiu" v-if="items.Match_DxDpl!=null&&items.Match_DxDpl!='0'">{{items.Match_DxGG1}}</span>
+                        <a href="javascript:"title="大" v-if="items.Match_DxDpl!=null&&items.Match_DxDpl!='0'"><font>{{items.Match_DxDpl}}</font></a>
                       </td>
                       <td style="width: 49px;" align="right">
-                        <span class="qiu">单</span>
-                        <a href="javascript:" title="单"><font>{{items.Match_DsDpl}}</font></a>
+                        <span class="qiu" v-if="items.Match_DsDpl!=null&&items.Match_DsDpl!='0'">单</span>
+                        <a href="javascript:" title="单" v-if="items.Match_DsDpl!=null&&items.Match_DsDpl!='0'"><font>{{items.Match_DsDpl}}</font></a>
                       </td>
                       <td style="width: 36px;" align="right" class="b_1st">
-                        <a href="javascript:"  title=""><font>{{items.Match_Bmdy}}</font></a>
+                        <a href="javascript:"  v-if="items.Match_Bmdy!=null&&items.Match_Bmdy!='0'"><font>{{items.Match_Bmdy}}</font></a>
                       </td>
                       <td style="width: 91px;" align="right" class="b_1st">
-                        <a href="javascript:"  title=""><font>{{items.Match_BHo}}</font></a>
+                        <span class="qiu" v-if="items.Match_BHo!=null&&items.Match_BHo!='0'">{{items.Match_ShowType == 'H' && items.Match_Ho != '0' ? items.Match_RGG : ''}}</span>
+                        <a href="javascript:"   v-if="items.Match_BHo!=null&&items.Match_BHo!='0'"><font>{{items.Match_BHo}}</font></a>
                       </td>
                       <td style="width: 88px;" align="right" class="b_1st">
-                        <span class="qiu">{{items.Match_Bdxpk1}}</span>
-                        <a href="javascript:"  title="大"><font>{{items.Match_Bdpl}}</font></a>
+                        <span class="qiu" v-if="items.Match_Bdpl!=null&&items.Match_Bdpl!='0'">{{items.Match_Bdxpk1}}</span>
+                        <a href="javascript:"  title="大" v-if="items.Match_Bdpl!=null&&items.Match_Bdpl!='0'"><font>{{items.Match_Bdpl}}</font></a>
                       </td>
                     </tr>
                     <tr>
                       <td align="right">
-                        <a href="javascript:"  title=""><font>{{items.Match_BzG}}</font></a>
+                        <a href="javascript:"  :title="items.Match_BzG" v-if="items.Match_BzG!=null&&items.Match_BzG!='0'"><font>{{items.Match_BzG}}</font></a>
                       </td>
                       <td align="right">
-                        <span class="qiu">{{items.Match_ShowType=='C'&&items.Match_Ao!='0'?items.Match_RGG:''}}</span>
-                        <a href="javascript:"  title=""><font>{{items.Match_Ao}}</font></a>
+                        <span class="qiu" v-if="items.Match_Ao!=null&&items.Match_Ao!='0'">{{items.Match_ShowType=='C'&&items.Match_Ao!='0'?items.Match_RGG:''}}</span>
+                        <a href="javascript:"  v-if="items.Match_Ao!=null&&items.Match_Ao!='0'"><font>{{items.Match_Ao}}</font></a>
                       </td>
                       <td align="right">
-                        <span class="qiu">{{items.Match_DxGG2}}</span>
-                        <a href="javascript:"  title="小"><font>{{items.Match_DxXpl}}</font></a></td>
+                        <span class="qiu" v-if="items.Match_DxXpl!=null&&items.Match_DxXpl!='0'">{{items.Match_DxGG2}}</span>
+                        <a href="javascript:"  title="小" v-if="items.Match_DxXpl!=null&&items.Match_DxXpl!='0'"><font>{{items.Match_DxXpl}}</font></a></td>
                       <td align="right">
-                        <span class="qiu">双</span>
-                        <a href="javascript:" title="双"><font>{{items.Match_DsSpl}}</font></a>
+                        <span class="qiu" v-if="items.Match_DsDpl!=null&&items.Match_DsDpl!='0'">双</span>
+                        <a href="javascript:" title="双"  v-if="items.Match_DsSpl!=null&&items.Match_DsSpl!='0'"><font>{{items.Match_DsSpl}}</font></a>
                       </td>
                       <td align="right" class="b_1st">
-                        <a href="javascript:"  title="洛克达尔"><font>{{items.Match_Bgdy1}}</font></a>
+                        <a href="javascript:" v-if="items.Match_Bgdy1!=null&&items.Match_Bgdy1!='0'"><font>{{items.Match_Bgdy1}}</font></a>
                       </td>
                       <td align="right" class="b_1st">
-                        <span class="qiu">{{items.Match_ShowType=='C'&&items.Match_Ao!='0'?items.Match_RGG:''}}</span>
-                        <a href="javascript:" title="洛克达尔"><font>{{items.Match_BAo}}</font></a>
+                        <span class="qiu" v-if="items.Match_BAo!=null&&items.Match_BAo!='0'">{{items.Match_ShowType=='C'&&items.Match_Ao!='0'?items.Match_RGG:''}}</span>
+                        <a href="javascript:" v-if="items.Match_BAo!=null&&items.Match_BAo!='0'"><font>{{items.Match_BAo}}</font></a>
                       </td>
                       <td align="right" class="b_1st">
-                        <span class="qiu">{{items.Match_Bdxpk2}}</span>
-                        <a href="javascript:" title="小"><font>{{items.Match_Bxpl}}</font></a>
+                        <span class="qiu" v-if="items.Match_Bdpl!=null&&items.Match_Bdpl!='0'">{{items.Match_Bdxpk2}}</span>
+                        <a href="javascript:" title="小" v-if="items.Match_Bdpl!=null&&items.Match_Bdpl!='0'"><font>{{items.Match_Bxpl}}</font></a>
                       </td>
                     </tr>
                     <tr >
                       <td align="left">和局</td>
                       <td align="right">
-                        <a href="javascript:"title="和局"><font>{{items.Match_BzH}}</font></a>
+                        <a href="javascript:"title="和局" v-if="items.Match_BzH!=null&&items.Match_BzH!='0'"><font>{{items.Match_BzH}}</font></a>
                       </td>
                       <td colspan="3"></td>
                       <td align="right" class="b_1st">
-                        <a href="javascript:" title="和局"><font>{{items.Match_Bhdy1}}</font></a>
+                        <a href="javascript:" title="和局" v-if="items.Match_Bhdy1!=null&&items.Match_Bhdy1!='0'"><font>{{items.Match_Bhdy1}}</font></a>
                       </td>
                       <td colspan="2" class="b_1st"></td>
                     </tr>
